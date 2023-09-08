e4577c77730881a8b5a201852528b3f4b68a1f96
Your Name <you@example.com>
2023-09-08 07:33:08 -0700

    ~

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there
+ * main: is for validating if the number ins positive
+ * Return:  style doc for function main goes there
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive", n);
+}
+else if (n == 0)
+{
+printf("%d is zero", n);
+}
+/* if the number number is not positive or zero is negative */
+else
+{
+printf("%d is negative", n);
+}
+return (0);
+}
