0303536f879f6c1bf59451e9bace0a654e484b17
Your Name <you@example.com>
2023-09-08 16:30:00 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c = '01';
+int d = '10';
+for ( ; c < '10'; c++)
+{
+putchar(c);
+for ( ; d <= '99'
+if (c <'10' && d < '99')
+{
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
