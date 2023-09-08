3e5a587be129f5543341ed85670c5ec3e16483fe
Your Name <you@example.com>
2023-09-08 07:43:28 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,16 +12,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 /* if the number number is not positive or zero is negative */
 else
 {
-printf("%d is negative", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
