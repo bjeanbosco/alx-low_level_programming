48bb10709ffb112c622875700b99ffd5d06160e9
Your Name <you@example.com>
2023-09-12 11:14:29 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _abs - prints the absolute value of a number
+*@r: number targeted
+*Return: returns 0
+*/
+int _abs(int r)
+{
+if (r > 0)
+{
+return (r);
+}
+else if (r < 0)
+return (-r);
+else
+return (0);
+}
