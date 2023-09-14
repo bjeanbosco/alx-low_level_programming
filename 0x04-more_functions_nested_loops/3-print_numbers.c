0f171cdecba9649504baea882e08585330fdac19
Your Name <you@example.com>
2023-09-14 04:35:26 -0700

    this task is for printing all number from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers - prints
+* Jean Bosco  owner
+* Return: void
+**/
+void print_numbers(void)
+{
+int i = '0';
+for ( ; i < '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
