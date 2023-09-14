b9cb6ca6a4285345ffda4fc9de92c8190f53c5a6
Your Name <you@example.com>
2023-09-14 09:56:10 -0700

    how good to be solved

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,21 @@ for (i = 1; i <= 100; i++)
 if ((i % 3 != 0) && (i % 5 != 0))
 {
 printf("%d", i);
-printf(" ");
 }
 else if ((i % 3 == 0) && (i % 5 == 0))
 {
 printf("FizzBuzz");
-printf(" ");
 }
 else if (i % 3 == 0)
 {
 printf("Fizz");
-printf(" ");
 }
 else
 {
 printf("Buzz");
+}
+if(i < 100)
+{
 printf(" ");
 }
 }
