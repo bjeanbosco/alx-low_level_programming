17fb63f6523b47b0f6f5898a358f8771c9784b18
Your Name <you@example.com>
2023-09-18 06:53:52 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*_strcpy - this is how we copy string to another variable
+*@dest: where to be copied
+*@src: srting to be copied
+*Return: copied string
+**/
+char *_strcpy(char *dest, char *src)
+{
+return (strcpy(dest, src));
+}
