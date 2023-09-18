386de11b462f820907b6201e95118373123a402c
Your Name <you@example.com>
2023-09-18 05:50:42 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * print_rev - prints a string, in reverse, followed by a new line.
- * @s: input string.
+ * print_rev - prints a revrsed string, 
+ * @s: string to be revesed
  * Return: no return.
  */
 void print_rev(char *s)
