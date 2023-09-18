21c9b53ee021eb9131d0dc331a0322d5f3bf1cc8
Your Name <you@example.com>
2023-09-18 04:03:01 -0700

    checking the length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strlen - this print length of string
+*@s: this is string bo be count
+*Return: str
+**/
+
+int _strlen(char *s)
+{
+return (strlen(s));
+}
