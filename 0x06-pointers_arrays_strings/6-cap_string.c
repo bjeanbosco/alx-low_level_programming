34ae881c3e7a281449d4cad3b4202015b11435a5
Your Name <you@example.com>
2023-09-21 07:02:37 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
 char *cap_string(char *str)
 {
 int i;
-for (i = 0; str[i] != '\0'; i++)
+for (i = 0; str[i]; i++)
 {
 if (i == 0)
 {
@@ -16,7 +16,7 @@ if (str[i] >= 'a' && str[i] <= 'z')
 str[i] = str[i] - 32;
 }
 }
-else if (str[i - 1] == ' ' || str[i - 1] == '.')
+else if (str[i - 1] == ' ' || str[i - 1] == '.' || str[i - 1] == ';' || str[i - 1] == ',' || str[i - 1] == '?' || str[i - 1] == '!' || str[i - 1] == '\"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i -1] == '}' || str[i - 1] == '\n')
 {
 if (str[i] >= 'a' && str[i] <= 'z')
 {
