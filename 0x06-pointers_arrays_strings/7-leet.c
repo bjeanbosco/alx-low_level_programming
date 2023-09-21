b79e10f783d4bf33e529ec2e237e6b0dacbb6b17
Your Name <you@example.com>
2023-09-21 08:26:49 -0700

    error solved

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,16 +9,18 @@
 
 char *leet(char *str)
 {
-char word[] = "aeotlAEOLTL";
-char code[] = "4307143071";
-int i = 0, j;
+char a[] = "aeotlAEOTL";
+char n[] = "4307143071";
+int i = 0;
+int j;
+
 while (str[i] != '\0')
 {
 for (j = 0; j <= 9; j++)
 {
-if (str[i] == word[j])
+if (str[i] == a[j])
 {
-str[i] = code[j];
+str[i] = n[j];
 }
 }
 i++;
