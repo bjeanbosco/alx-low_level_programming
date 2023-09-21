5ae372098ddc273876cd813242fc333f32525ac6
Your Name <you@example.com>
2023-09-21 07:25:07 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,31 +6,27 @@
 **/
 char *cap_string(char *str)
 {
-int i;
+int i, j;
+int p[] = {9, 32, 10, 40, 125, 123, 41, 34, 59, 46, 33, 44, 63};
 for (i = 0; str[i]; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
 {
 if (i == 0)
 {
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 32;
-}
-}
-else if (str[i - 1] == ' ' || str[i - 1] == '.' || str[i - 1] == ';' || str[i - 1] == ',' || str[i - 1] == '?' || str[i - 1] == '!' || str[i - 1] == '\"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i -1] == '}' || str[i - 1] == '\n')
-{
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 32;
-}
 
+str[i] = str[i] - 32;
 }
 else
 {
-if (str[i] >= 'a' && str[i] <= 'z')
+for (j = 0; j <= 12; j++)
 {
-str[i] = str[i];
+if (p[j] == str[i - 1])
+{
+str[i] = str[i] - 32;
 }
-
+}
+}
 }
 }
 return (str);
