931e8353a61926c803e85a02bc7e4212db8eccc2
Your Name <you@example.com>
2023-09-25 04:25:24 -0700

    pointer to the first occurrence of the character c in the string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+* *_strchr - this is used for search first occurence
+* @s: string to be scanned or in which to search
+* @c: character searched from the string
+* Return: on success 1
+**/
+
+char *_strchr(char *s, char c)
+{
+return (strchr(s, c));
+
+}
