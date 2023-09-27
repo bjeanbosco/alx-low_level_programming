d3bf14b7b664d5521b7eab1c38adb4cde4390207
Your Name <you@example.com>
2023-09-27 13:45:07 -0700

    squareroot

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,31 @@
 #include "main.h"
 #include <stdio.h>
 #include <math.h>
+
+/**
+ * sqr - sqr
+ * @x: number 1
+ * @y: number
+ *Return: on success 1
+ *on error return -1
+ **/
+
+
+int sqr(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+	else if (y * y > x)
+		return (-1);
+	return (sqr(x, y + 1));
+}
 /**
 * _sqrt_recursion - sqrt
 * @n: number
 * Return: on success 1
-* on error return -1
+* on error retur
 **/
-
-int _sqrt_recursion(int n, int x)
+int _sqrt_recursion(int n)
 {
-if (n * n == x)
-return (n);
-else if (n * n > x)
-return (-1);
-return (_sqrt_recursion(x, n + 1));
+	return (sqr(n, 1));
 }
