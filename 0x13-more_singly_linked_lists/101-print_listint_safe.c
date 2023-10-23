80fc23ba9896eb60fde35cbcb72f2916f7572244
Your Name <you@example.com>
2023-10-23 11:05:10 -0700

    descriptiion

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * reverse_listint - prints a listint_t linked list.
+ * reverse_listint_safe - prints a listint_t linked list.
  * @head: pointer to the list.
  * Return: number of nodes in the list.
  **/
